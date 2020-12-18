2f7687d1598ce44a812c74011a06cbfb0d0577a7
KimuGenie <baby11686@naver.com>
2020-12-18 22:17:46 +0900

    upsampling 수정
    train/test set을 먼저 나누고 train set을 upsamplinig해주는 방식으로 바꿈

--- Ensemble classifier.py
+++ Ensemble classifier.py
@@ -16,17 +16,14 @@ from sklearn.utils import resample
 df = pd.read_csv(os.path.abspath('dataset.csv'),header=None)
 y = df.iloc[:, 11].values
 X = df.iloc[:, [2, 3, 4, 5]].values #SBS에서 가장 정확도가 높았던 네 개의 feature
-X_org = X #upsampling 하지 않을 원본 데이터
-y_org = y
-
-# class 1과 0의 비율을 1:1로 upsampling함. 총 9040개의 데이터를 사용함.
-X_upsampled, y_upsampled = resample(X[y == 1], y[y == 1], replace=True, n_samples=X[y == 0].shape[0], random_state=1)
-X = np.vstack((X[y==0], X_upsampled))
-y = np.hstack((y[y==0], y_upsampled))
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y) #30% test set
-X_train_org, X_test_org, y_train_org, y_test_org = train_test_split(X_org, y_org, test_size=0.3, random_state=1, stratify=y_org)
+
+# train set을 upsampling해줌 총 6328개의 데이터를 훈련에 사용
+X_upsampled, y_upsampled = resample(X_train[y_train == 1], y_train[y_train == 1], replace=True, n_samples=X_train[y_train == 0].shape[0], random_state=1)
+X_train = np.vstack((X_train[y_train==0], X_upsampled))
+y_train = np.hstack((y_train[y_train==0], y_upsampled))
 ################################################################################
 
 #################### Majority Vote Classifier ##########################
@@ -91,17 +88,18 @@ esb = MajorityVoteClassifier(classifiers=[pipeKNN, tree, pipeSV])
 
 esb.fit(X_train, y_train)
 
+y_test_pred = esb.predict(X_test)
 
 ##### score#####
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
-print("train/test accuracy: %0.4f/%0.4f" %(accuracy_score(y_train_org, esb.predict(X_train_org)), accuracy_score(y_test_org, esb.predict(X_test_org))))
-print("ROCAUC Score: %0.4f" % (roc_auc_score(y_test_org, esb.predict(X_test_org))))
+print("train/test accuracy: %0.4f/%0.4f" %(accuracy_score(y_train, esb.predict(X_train)), accuracy_score(y_test, y_test_pred)))
+print("ROCAUC Score: %0.4f" % (roc_auc_score(y_test, y_test_pred)))
 
 ###### confusion matrix ######
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-confmat = confusion_matrix(y_true=y_test_org, y_pred=esb.predict(X_test_org))
+confmat = confusion_matrix(y_true=y_test, y_pred=y_test_pred)
 fig, ax = plt.subplots(figsize=(2.5, 2.5))
 ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.5)
 for i in range(confmat.shape[0]):
